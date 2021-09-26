cbdd8e2339599c0cd8cd76b2578003383443815e
Rassel <Rassel>
2021-09-26 20:13:57 +0300

    update

--- rembrain_robot_framework/processes/__init__.py
+++ rembrain_robot_framework/processes/__init__.py
@@ -3,6 +3,7 @@ from .command_receiver import CommandReceiver
 from .command_sender import CommandSender
 from .ping_sender import PingSender
 from .state_receiver import StateReceiver
+from .stub import StubProcess
 from .video_packer import VideoPacker
 from .video_receiver import VideoReceiver
 from .ws import WsRobotProcess
