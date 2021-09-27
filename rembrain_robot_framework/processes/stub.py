be57172144aae001ebd466150e43c9875301f3b7
Rassel <Rassel>
2021-09-27 19:02:49 +0300

    update all process

--- rembrain_robot_framework/processes/stub.py
+++ rembrain_robot_framework/processes/stub.py
@@ -8,7 +8,7 @@ class StubProcess(RobotProcess):
     """ It is just stub without any benefit work."""
 
     def run(self):
-        logging.info(f"Started video receiver process, name: {self.name}")
+        logging.info(f"{self.__class__.__name__} started, name: {self.name}.")
 
         while True:
             sleep(20)
