d1ec7257349b4e05b83ac282f61dd330c5116bd0
Kirill Bolashev <kirik1007@gmail.com>
2021-12-03 14:50:03 +0300

    Bumped version to 0.0.28

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'config', 'requirements', 'bas
 
 setuptools.setup(
     name="rembrain_robot_framework",
-    version="0.0.27",
+    version="0.0.28",
     author="Rembrain",
     author_email="info@rembrain.ai",
     description="Rembrain Robot Framework",
