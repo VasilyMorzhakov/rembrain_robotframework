effea9834ebe3145c79630accfd372cf63fd0d1d
Kirill Bolashev <kirik1007@gmail.com>
2022-01-21 11:52:07 +0300

    Bumped doc version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Rembrain"
 author = "Rembrain"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.1.1"
+release = "v0.1.4"
 
 
 # -- General configuration ---------------------------------------------------
