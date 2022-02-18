16997512efe8a59d95c91532e1ed4f8916dd1514
Rassel <Rassel>
2022-02-18 16:20:50 +0300

    fix vers

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Rembrain"
 author = "Rembrain"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.1.5"
+release = "v0.1.6"
 
 
 # -- General configuration ---------------------------------------------------
