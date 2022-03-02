0c5d6c69055a18072078fbb74c7c15d65d8f2dce
Rassel <Rassel>
2022-03-02 21:04:22 +0300

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Rembrain"
 author = "Rembrain"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.8"
+release = "0.1.10"
 
 # -- General configuration ---------------------------------------------------
 
