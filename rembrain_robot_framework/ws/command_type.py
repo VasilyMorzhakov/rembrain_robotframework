d8fd60d6903d5ddb4f5852e29ebb0359c6d56d53
dshepelev15 <d-shepelev@list.ru>
2024-10-22 19:51:30 +0200

    Add a new ws command type

--- rembrain_robot_framework/ws/command_type.py
+++ rembrain_robot_framework/ws/command_type.py
@@ -3,5 +3,6 @@ class WsCommandType:
     PUSH = "push"
     PUSH_LOOP = "push_loop"
     PING = "ping"
+    BIDIRECTIONAL = 'bidirectional'
 
-    ALL_VALUES = (PULL, PUSH, PUSH_LOOP, PING)
+    ALL_VALUES = (PULL, PUSH, PUSH_LOOP, PING, BIDIRECTIONAL)
